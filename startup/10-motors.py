6616da1f5bbc581be317ea2bc57ecb8c22db82c4
Stuart Campbell <stuart@stuartcampbell.me>
2016-11-23 17:32:16 -0500

    fix typos

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -19,14 +19,14 @@ bs_a2b = EpicsMotor('BL14B:Mot:a2b', name='bs_a2b')
 bs_s1 = EpicsMotor('BL14B:Mot:s1', name='bs_s1')
 
 # Sample translation upper (y), lower (x)
-bs_stu = EpicsMotor('BL14B:Mot:stu', name='bs_stu)
-bs_stl = EpicsMotor('BL14B:Mot:stl', name='bs_stl)
+bs_stu = EpicsMotor('BL14B:Mot:stu', name='bs_stu')
+bs_stl = EpicsMotor('BL14B:Mot:stl', name='bs_stl')
 
 # Sample goniometer upper (y), lower (x) tilt
-bs_sgu = EpicsMotor('BL14B:Mot:sgu', name='bs_sgu)
-bs_sgl = EpicsMotor('BL14B:Mot:sgl', name='bs_sgl)
+bs_sgu = EpicsMotor('BL14B:Mot:sgu', name='bs_sgu')
+bs_sgl = EpicsMotor('BL14B:Mot:sgl', name='bs_sgl')
 
 # Sample Axes (stick on sample environment, etc)
-bs_axis1 = EpicsMotor('BL14B:Mot:Axis1', name='bs_Axis1)
-bs_axis2 = EpicsMotor('BL14B:Mot:Axis2', name='bs_Axis2)                      
+bs_axis1 = EpicsMotor('BL14B:Mot:Axis1', name='bs_Axis1')
+bs_axis2 = EpicsMotor('BL14B:Mot:Axis2', name='bs_Axis2')                      
  

9a8d583a1b0ba22732c229c2a652f11a4b772c4a
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-05 10:59:06 -0500

    Update 50-scans.py

--- startup/50-scans.py
+++ startup/50-scans.py
@@ -4,7 +4,7 @@ def waitfor_proton_charge(threshold):
     """Set, trigger, read until the current reaches threshold"""
     i = 0
     while True:
-        print("LOOP %d" % i)
+        print("Waiting for Proton Charge (> %f) LOOP %d" % (threshold, i))
         yield from trigger(bs_pcharge, wait=True)
         current_pcharge = yield from read(bs_pcharge)
         if current_pcharge['bs_pcharge']['value'] >= threshold:

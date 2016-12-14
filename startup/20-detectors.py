1f6a734907ef4a959cb8e38f3973da81088aed3b
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-14 16:40:31 -0500

    Intial feable attempt at defining a new detector

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -1,6 +1,42 @@
-from ophyd import (EpicsScaler, EpicsSignal, EpicsSignalRO, Device, StatsPlugin)
+from ophyd import (EpicsScaler, EpicsSignal, EpicsSignalRO,
+                   Device, DeviceStatus)
 from ophyd import Component as Cpt
 
+from bluesky.examples import NullStatus
+
+class NeutronDetector(Device):
+    start_cmd = Cpt(EpicsSignal, ':CS:RunControl:Start')
+    pause_cmd = Cpt(EpicsSignal, ':CS:RunControl:Pause')
+    stop_cmd = Cpt(EpicsSignal, ':CS:RunControl:Stop')
+
+    def kickoff(self):
+
+        # Callback will not work with simulation run control
+        self.stop_cmd.put(1)
+        #self.stop_cmd.put(1,wait=True)
+        status = DeviceStatus(self)
+        return status
+
+    def complete(self):
+        status = DeviceStatus(self)
+        return NullStatus
+
+    def pause(self):
+        # Callback will not work with simulation run control
+        self.pause_cmd.put(1)
+        #self.pause_cmd.put(1,wait=True)
+
+    def resume(self):
+        # Callback will not work with simulation run control
+        self.pause_cmd.put(0)
+        #self.pause_cmd.put(0,wait=True)
+
+    def stop(self):
+        # Callback will not work with simulation run control
+        self.stop_cmd.put(1)
+        #self.stop_cmd.put(1,wait=True)
+
+adara_detector = NeutronDetector('BL99', name='adara_detector')
 
 # integrated proton charge for the run
 bs_pcharge = EpicsSignalRO('BL14B:Det:N1:PChargeIntegrated_RBV', name='bs_pcharge')

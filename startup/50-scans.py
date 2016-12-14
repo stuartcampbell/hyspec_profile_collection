bc26df24caa730457f818cd7c31099670f23f3cd
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-14 17:03:15 -0500

    Added time based 'fly scan'

--- startup/50-scans.py
+++ startup/50-scans.py
@@ -1,6 +1,7 @@
-from bluesky.plans import abs_set, trigger, read
+import bluesky.plans as bp
+from bluesky.plans import abs_set, trigger, read, run_decorator
 
-@bluesky.run_decorator(plan_name='waitfor_proton_charge')
+@run_decorator(plan_name='waitfor_proton_charge')
 def waitfor_proton_charge(threshold):
     """Set, trigger, read until the current reaches threshold"""
     i = 0
@@ -12,3 +13,10 @@ def waitfor_proton_charge(threshold):
             print('DONE')
             break
         i += 1
+
+@run_decorator(plan_name='pump_plan')
+def time_plan(collection_time):
+    yield from bp.kickoff(adara_detector, wait=True)
+    yield from bp.sleep(collection_time)
+    yield from bp.complete(adara_detector, wait=True)
+    # yield from bp.collect(adara_detector)  # they don't actually do this step, but you should

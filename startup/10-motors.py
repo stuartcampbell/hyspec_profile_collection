17c218dbe2db648063e5920917bb3da44ff7ff27
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-16 14:16:38 -0500

    Added more motors

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -32,5 +32,26 @@ bs_sgl = EpicsMotor('BL14B:Mot:sgl', name='bs_sgl')
 
 # Sample Axes (stick on sample environment, etc)
 bs_axis1 = EpicsMotor('BL14B:Mot:Axis1', name='bs_Axis1')
-bs_axis2 = EpicsMotor('BL14B:Mot:Axis2', name='bs_Axis2')                      
- 
+bs_axis2 = EpicsMotor('BL14B:Mot:Axis2', name='bs_Axis2')
+
+# Vertical elevator, selects monochromators
+bs_vmel_pos = EpicsMotor('BL14B:Mot:vmel:Position', name='bs_vmel_pos')
+
+# Pyrolitic Grpahite: top z-rotation, top focus
+bs_m1pg = EpicsMotor('BL14B:Mot:m1pg', name='bs_m1pg')
+bs_mfpg = EpicsMotor('BL14B:Mot:mfpg', name='bs_mfpg')
+
+# Heusler: bottom z-rotation, bottom focus
+bs_m1hu = EpicsMotor('BL14B:Mot:m1hu', name='bs_m1hu')
+bs_mfhu = EpicsMotor('BL14B:Mot:mfhu', name='bs_mfhu')
+
+# Drum shield sample table
+bs_vmsd = EpicsSignal('BL14B:Mot:msd:EncoderPos', name='bs_vmsd')
+
+# Virtual drum shield
+bs_vm2 = EpicsMotor('BL14B:Mot:vm2', name='bs_vm2')
+
+# Virtual detector vessel
+bs_vs2 = EpicsSignal('BL14B:Mot:s2:ProfibusEncoder', name='bs_vs2')
+
+bs_vmotor_error = EpicsSignal('BL14B:Mot:Seq1:Error', name='bs_vmotor_error')

c854808b2fb24126f472ff86236579bcab9ef228
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-18 17:22:32 -0500

    Create analysis.py

---
+++ scripts/analysis.py
@@ -0,0 +1,14 @@
+# I've setup Metadatastore config in ~/.config/metadatastore/connection.yml
+# I've setup Filestore config in ~/.config/filestore/connection.yml
+
+from databroker import db
+
+# Find run number 154338
+db(scan_id=154338)
+
+# All started on sunday
+db(start_time='2016-12-18')
+
+# All started/stopped on sunday
+db(start_time='2016-12-18', stop_time='2015-12-18')
+

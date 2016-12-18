df73b65fcdc2df5f7bb2a2469635998050271d4a
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-18 15:39:31 -0500

    Don't care about x-errors!

--- startup/05-classes.py
+++ startup/05-classes.py
@@ -3,29 +3,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def adjustErrbarxy(self, errobj, x, y, x_error, y_error):
+def adjustErrbarxy(self, errobj, x, y, y_error):
     ln, (errx_top, errx_bot, erry_top, erry_bot), (barsx, barsy) = errobj
     x_base = x
     y_base = y
 
-    xerr_top = x_base + x_error
-    xerr_bot = x_base - x_error
+    #xerr_top = x_base + x_error
+    #xerr_bot = x_base - x_error
     yerr_top = y_base + y_error
     yerr_bot = y_base - y_error
 
-    errx_top.set_xdata(xerr_top)
-    errx_bot.set_xdata(xerr_bot)
-    errx_top.set_ydata(y_base)
-    errx_bot.set_ydata(y_base)
+    #errx_top.set_xdata(xerr_top)
+    #errx_bot.set_xdata(xerr_bot)
+    #errx_top.set_ydata(y_base)
+    #errx_bot.set_ydata(y_base)
 
     erry_top.set_xdata(x_base)
     erry_bot.set_xdata(x_base)
     erry_top.set_ydata(yerr_top)
     erry_bot.set_ydata(yerr_bot)
 
-    new_segments_x = [np.array([[xt, y], [xb,y]]) for xt, xb, y in zip(xerr_top, xerr_bot, y_base)]
+    #new_segments_x = [np.array([[xt, y], [xb,y]]) for xt, xb, y in zip(xerr_top, xerr_bot, y_base)]
     new_segments_y = [np.array([[x, yt], [x,yb]]) for x, yt, yb in zip(x_base, yerr_top, yerr_bot)]
-    barsx.set_segments(new_segments_x)
+    #barsx.set_segments(new_segments_x)
     barsy.set_segments(new_segments_y)
 
 
@@ -135,7 +135,7 @@ class LivePlotWithErrors(CallbackBase):
 
     def update_plot(self):
         # self.current_line.set_data(self.x_data, self.y_data)
-        adjustErrbarxy(self.current_line, self.x_data, self.y_data, None, self.e_data)
+        adjustErrbarxy(self.current_line, self.x_data, self.y_data, self.e_data)
         # Rescale and redraw.
         self.ax.relim(visible_only=True)
         self.ax.autoscale_view(tight=True)

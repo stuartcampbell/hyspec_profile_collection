a962786e2997af6c8d044ff05971238945ab6e13
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-18 17:41:04 -0500

    Update analysis.py

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -12,3 +12,7 @@ db(start_time='2016-12-18')
 # All started/stopped on sunday
 db(start_time='2016-12-18', stop_time='2015-12-18')
 
+# Get the 'db_neutrons' data into a pandas dataframe
+h = db(scan_id=154338)
+t = db.get_table(h,fields=['bs_neutrons'])
+

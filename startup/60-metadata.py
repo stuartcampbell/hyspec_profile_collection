05b564bf21449d34a53fda193f287a6a185a8ddd
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-19 09:57:32 -0500

    Logic to deal with run number correctly
    If we are currently running then we don't need to subtract 1 from the run number.
    When we are paused - the adara run number is 0

--- startup/60-metadata.py
+++ startup/60-metadata.py
@@ -4,7 +4,10 @@
 RE.md['beamline_id'] = 'HYSPEC'
 RE.md['proposal_id'] = None
 # Have to do this as bluesky will advance it on it's own, as does ADARA.
-RE.md['scan_id'] = bs_run_number.value - 1
+if bs_adara_run_number.value == 0:
+    RE.md['scan_id'] = bs_run_number.value
+else:
+    RE.md['scan_id'] = bs_run_number.value - 1    
 
 # Add a callback that prints scan IDs at the start of each scan.
 

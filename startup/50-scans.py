52e981257c5e056b7accff103cfc4a1bd891092c
Thomas A Caswell <tcaswell@bnl.gov>
2016-12-19 10:32:20 -0500

    MNT: use fly_during_decorator

--- startup/50-scans.py
+++ startup/50-scans.py
@@ -1,5 +1,5 @@
 import bluesky.plans as bp
-from bluesky.plans import abs_set, trigger, read, run_decorator
+from bluesky.plans import abs_set, trigger, read, run_decorator, fly_during_decorator
 
 
 @run_decorator()
@@ -26,13 +26,7 @@ def time_plan(collection_time):
     yield from bp.collect(detector)
 
 
-@run_decorator()
-def pcharge_plan(pcharge):
-    '''Plan to collection for a given beam current'''
-    yield from bp.kickoff(detector, wait=True)
-    yield from waitfor_proton_charge(pcharge)
-    yield from bp.complete(detector, wait=True)
-    yield from bp.collect(detector)
+pcharge_plan = fly_during_decorator([detector])(waitfor_proton_charge)
 
 
 @run_decorator()

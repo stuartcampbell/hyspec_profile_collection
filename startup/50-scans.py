a918a3fdaddb38ca429ad9242ea99e811ce80b63
Stuart Campbell <stuart@stuartcampbell.me>
2016-12-19 14:57:37 -0500

    use the correct pause / resumes !

--- startup/50-scans.py
+++ startup/50-scans.py
@@ -63,14 +63,14 @@ def continuous_step_scan(mymotor, motor_min, motor_max,
     # Start
     yield from bp.kickoff(detector, wait=True)
     # and immediately pause
-    yield from bp.pause(detector)
+    yield from abs_set(detector.pause, 1)
 
     for num in np.arange(motor_min, motor_max, motor_step):
         yield from abs_set(mymotor, num, wait=True)
         yield from abs_set(bs_adned_reset_counters, 1, wait=True)
-        yield from bp.resume(detector)
+        yield from abs_set(detector.resume, 1, wait=True)
         yield from bp.sleep(collection_time)
-        yield from bp.pause(detector)
+        yield from abs_set(detector.pause, 1)
 
     yield from bp.complete(detector, wait=True)
     yield from bp.collect(detector)
